51578ce1238441d52707df17119ac190b76c4f19
Thomas Bachman <tbachman@yahoo.com>
2015-08-07 08:39:12 -0400

    Fix step00.sh script.
    There was an extra "done" in the step00.sh script.
    This patch removes it.
    
    Signed-off-by: Thomas Bachman <tbachman@yahoo.com>

--- devstack-scripts/tutorial/step00.sh
+++ devstack-scripts/tutorial/step00.sh
@@ -3,7 +3,7 @@ source openrc admin admin
 # create ssh key for each tenant
 # 
 for x in admin ; do echo "ssh key for ${x}" ; \
-   rm -f id_rsa_${x}* ; ssh-keygen -t rsa -b 2048 -N '' -f id_rsa_${x} ; done
+   rm -f id_rsa_${x}* ; ssh-keygen -t rsa -b 2048 -N '' -f id_rsa_${x}
    nova keypair-add --pub-key  id_rsa_${x}.pub  ${x}_key
 done
 
